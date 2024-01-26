27116e2391fcd37ebd252b3154aef5cdb7d7b13c
MaciejSowiar <maciej.sowiar@reality.co>
2024-01-26 21:33:56 +0100

    Camera follow added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothSpeed = 0.125f;
+    [SerializeField] Vector3 locationOffset;
+    [SerializeField] Vector3 rotationOffset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + target.rotation * locationOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        Quaternion desiredrotation = target.rotation * Quaternion.Euler(rotationOffset);
+        Quaternion smoothedrotation = Quaternion.Lerp(transform.rotation, desiredrotation, smoothSpeed);
+        transform.rotation = smoothedrotation;
+    }
+}
