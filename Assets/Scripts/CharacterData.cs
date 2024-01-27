d198dfb9095da6f889475294826f827b9d78b606
tallyon <skowronski6@gmail.com>
2024-01-27 12:47:36 +0100

    asdas

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -7,6 +7,17 @@ public class CharacterData : ScriptableObject
     [SerializeField] private CharacterTypeEnum characterType;
     [SerializeField] private GameObject prefab;
 
+    [Header("Ability 1")]
+    private Sprite ability1Sprite;
+    [SerializeField] private int ability1CooldownSeconds;
+    private AudioClip ability1Sound;
+    
+    [Header("Ability 2")]
+    private Sprite ability2Sprite;
+    [SerializeField] private int ability2CooldownSeconds;
+    private AudioClip ability2Sound; 
+    
+
     public GameObject Prefab => prefab;
     public CharacterTypeEnum CharacterType => characterType;
 }
