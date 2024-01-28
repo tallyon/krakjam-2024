7529d6667d1700b124a329d6266f612ee08fee53
tallyon <skowronski6@gmail.com>
2024-01-28 13:43:33 +0100

    gasnica uzycie na E, muzyka

--- Assets/Scripts/VentAbilityConfig.cs
+++ Assets/Scripts/VentAbilityConfig.cs
@@ -5,4 +5,6 @@ public class VentAbilityConfig : AbilityConfig
 {
     [SerializeField] private float travelDuration;
     public float TravelDuration => travelDuration;
+    [SerializeField] private AudioClip sfx;
+    public AudioClip Sound => sfx;
 }
