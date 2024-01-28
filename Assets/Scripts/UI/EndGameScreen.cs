6947443704eb4502e29880fe08a81e21047d5654
tallyon <skowronski6@gmail.com>
2024-01-28 14:16:49 +0100

    added wind and lose sounds

--- Assets/Scripts/UI/EndGameScreen.cs
+++ Assets/Scripts/UI/EndGameScreen.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 
 namespace UI
 {
+    [RequireComponent(typeof(AudioSource))]
     public class EndGameScreen : MonoBehaviour
     {
         [SerializeField] private GameObject content;
         [SerializeField] private GameObject chad;
         [SerializeField] private GameObject nerd;
         [SerializeField] private GameObject noone;
+
+        [SerializeField] private AudioClip loseSound;
+        [SerializeField] private AudioClip winSound;
+        private AudioSource _audio;
+
+        private void Awake()
+        {
+            _audio = GetComponent<AudioSource>();
+        }
 
         private void Start()
         {
@@ -33,16 +43,19 @@ namespace UI
         private void ShowEndGameScreenPlayer1()
         {
             chad.SetActive(true);
+            if (_audio != null && winSound != null) _audio.PlayOneShot(winSound);
         }
         
         private void ShowEndGameScreenPlayer2()
         {
             nerd.SetActive(true);
+            if (_audio != null && winSound != null) _audio.PlayOneShot(winSound);
         }
 
         private void ShowEndGame()
         {
             noone.SetActive(true);
+            if (_audio != null && loseSound != null) _audio.PlayOneShot(loseSound);            
         }
 
         
